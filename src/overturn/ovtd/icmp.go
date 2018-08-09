321f6474ec1d0ab0a80d1d759bb7566e667dbd80
sunmxt <sunmxt@126.com>
2018-08-09 18:41:42 +0800

    ICMP backend.

--- src/overturn/ovtd/icmp.go
+++ src/overturn/ovtd/icmp.go
@@ -1,13 +1,189 @@
 package ovtd
 
 import (
-    "net"
+    "net/icmp"
+    "runtime"
+    "encoding/binary"
+    "sync/atomic"
+    "fmt"
+    "time"
+    log "github.com/sirupsen/logrus"
 )
 
-type ICMPTunnelWorker struct {
-     
+// Packet Encapsulation
+type OVTPacket []byte
+
+const (
+    OVT_MAGIC = [4]byte{'O', 'V', 'T', 0xAA}
+
+    OVT_HEADER_SIZE = 8
+
+    PKG_TOO_SHORT = "Packet too short."
+    PKG_INVALID = "Not a OVTPacket."
+)
+
+func NewOVTPacket(payload_size uint) OVTPacket {
+    pack := make(OVTPacket, payload_size + OVT_HEADER_SIZE)
+    copy(pack[0:4], OVT_MAGIC)
+    return pack
 }
 
-func NewICMPTunnelWorker() {
-    net.ListenPacket("ip4:icmp", "0.0.0.0")
+func OVTPacketUnpack(buf byte[], size_limit uint32) *OVTPacket, error {
+    if len(buf) < 4 {
+        return false, nil, fmt.Errorf(PKG_TOO_SHORT)
+    }
+    if buf[0:4] != OVT_MAGIC {
+        return false, nil, fmt.Errorf(PKG_INVALID)
+    }
+    if len(buf) < 8 {
+        return true, nil, fmt.Errorf(PKG_TOO_SHORT)
+    }
+    enc_len := binary.BigEndian.Uint32(buf[4:8])
+    if enc_len != len(buf) {
+        return true, nil, fmt.Errorf("Packet size unmatched. (buffed: %v, encoded: %v)", len(buf), enc_len)
+    }
+
+    pkt := NewOVTPacket(enc_len - OVT_HEADER_SIZE)
+    copy(pkt.PayloadRef, buf[OVT_HEADER_SIZE:])
+    return pkt
+}
+
+func (pack OVTPacket) MagicRef() OVTPacket {
+    return pack[0:4]
+}
+
+func (pack OVTPacket) Pack() []byte {
+    binary.BigEndian.PutUint32(pack[4:8], len(pack))
+    return []byte(OVTPacket)
+}
+
+func (pack OVTPacket) PayloadRef() []byte {
+    return pack[4:len(OVTPacket)]
+}
+
+
+// Tunnel
+type ICMPTunnel struct {
+    DataIn          chan []*OVTPacket
+    DataOut         chan []*OVTPacket
+    MaxWorker       int
+    RxStat          uint64
+    WxStat          uint64
+    MTU             uint32
+
+    worker_count    uint32
+    running         uint32    
+    conn            icmp.PacketConn
+    sigStop         chan int
+}
+
+
+func NewICMPTunnel(address string) ICMPTunnel, error {
+    var err error
+    tun := new(ICMPTunnel)
+
+    tun.MaxWorker = runtime.NumCPU()
+    tun.worker_count = 0
+    tun.RxStat = 0
+    tun.WxStat = 0
+    tun.MTU = 1464 // Header: IP(20byte) + ICMP(8Byte) + OVT(8Byte)
+    tun.DataOut = nil
+    tun.DataIn = make(chan []byte, tun.MaxWorker)
+    tun.sigStop = make(chan int)
+    
+    tun.conn, err = net.ListenPacket("ip4:icmp", address)
+    if err != nil {
+        return nil, err
+    }
+    return tun
+}
+
+func (tun *ICMPTunnel) Destroy() error {
+    return tun.conn.Close()
+}
+
+func (tun *ICMPTunnel) addReader() error {
+    if tun.MaxWorker > 0 && len(tun.worker) >= tun.MaxWorker {
+        return fmt.Errorf("Maximum number of worker reached.")
+    }
+
+    atomic.AddUint32(&tun.worker_count, 1)
+    go func() {
+        var err error, pkt *OVTPacket, is_encap bool
+
+        buf := make([]byte, tun.MTU + 28 + OVT_HEADER_SIZE)
+
+        for tun.running {
+            now := timer.Now()
+            tun.conn.SetReadDeadline(now.Add(1000000000))
+            sz, from, err := tun.conn.ReadFrom(buf)
+
+            is_encap , pkt, err = OVTPacketUnpack(buf)
+            if is_encap {
+                if err != nil {
+                    log.WithFields(log.Fields{
+                            "module" : "ICMPTunnel",
+                            "event" : "packet",
+                            "err_detail" : "",
+                        }).Warningf("Invalid packet. Drop.")
+                    continue
+                }
+                atomic.AddUint64(&tun.RxStat, len(*pkt) - OVT_HEADER_SIZE)
+
+                tun.DataOut <- pkt
+            }
+        }
+
+        last := atomic.AddUint32(&tun.worker_count, uint32(-1))
+        if last == 0 {
+            tun.stopStop <- 0
+        }
+    }
+
+    return nil
+}
+
+func (tun *ICMPTunnel) Write(packet OVTPacket, address net.Addr) int, error {
+    wx, err := tun.conn.WriteTo(packet, address)
+    atomic.AddUint64(&tun.WxStat, wx)
+    return wx, err
+}
+
+func (tun *ICMPTunnel) Start() error {
+    var worker_count int
+
+    if tun.MaxWorker < -1 {
+        worker_count = runtime.NumCPU()
+    } else {
+        worker_count = tun.MaxWorker
+    }
+
+    tun.DataOut = make(chan []byte, tun.MaxWorker)
+    
+    for ; worker_count > 0 ; worker_count-- {
+        err := tun.addReader()
+        if err != nil {
+            return err
+        }
+    }
+    return nil
+}
+
+func (tun *ICMPTunneler) Stop() error {
+    for {
+        running := tun.running
+        if running == 0 {
+            return fmt.Errorf("Not running.")
+        }
+        if atomic.CompareAndSwapUint32(&tun.running, running, 0) {
+            break
+        }
+    }
+
+    close(tun.DataOut)
+
+    // wait until all readers are stopped
+    <- tun.sigStop
+
+    tun.DataOut = nil    
 }

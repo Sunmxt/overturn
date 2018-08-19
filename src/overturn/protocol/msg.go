7d07a38c5eff151a830129858cb001e913070fde
sunmxt <sunmxt@126.com>
2018-08-19 14:34:55 +0800

    initial encapsulating protocol.

--- src/overturn/protocol/msg.go
+++ src/overturn/protocol/msg.go
@@ -2,217 +2,155 @@ package protocol
 
 import (
 	"encoding/binary"
+	"errors"
 	"fmt"
 	"github.com/google/uuid"
 )
 
-type MessageHeader struct {
-	Major uint8
-	Minor uint8
-	Type  uint8
-}
-
-const MSG_HEADER_SIZE = 3
-
-func (m *MessageHeader) Marshal(buf []byte) {
-	buf[0] = m.Major
-	buf[1] = m.Minor
-	buf[2] = m.Type
-}
-
-func (m *MessageHeader) Unmarshal(buf []byte) error {
-	m.Major = buf[0]
-	m.Minor = buf[1]
-	m.Type = buf[2]
-	return nil
-}
-
 const (
-	NODE_ACTIVATE = iota
-	HEARTBEAT_MASTER
-	HEARTBEAT_NODE
-	JOIN_REQUEST
-)
-
-const (
-	VERSION_MAJOR = 1
-	VERSION_MINOR = 0
+	ERR_BUFFER_SMALL = "Buffer is too small."
 )
 
 type Message interface {
-	GetVersion() (uint8, uint8)
 	Type() uint8
 	Marshal() []byte
+	Place([]byte) error
 	Unmarshal([]byte) error
 	Size() uint
 }
 
 // Join : Join a network
 type JoinRequest struct {
-	Header MessageHeader
-	Name   [16]byte
-	Token  uuid.UUID
+	Name  [16]byte
+	Token uuid.UUID
 }
 
 func NewJoinRequest(network_name string, token uuid.UUID) *JoinRequest {
 	m := new(JoinRequest)
-	m.Header.Major = VERSION_MAJOR
-	m.Header.Major = VERSION_MINOR
-	m.Header.Type = JOIN_REQUEST
 	copy(m.Name[:], network_name)
 	m.Token = token
 	return m
 }
 
-func (m *JoinRequest) GetVersion() (uint8, uint8) {
-	return m.Header.Major, m.Header.Minor
-}
-
 func (m *JoinRequest) Type() uint8 {
-	return m.Header.Type
+	return JOIN_REQUEST
 }
 
 func (m *JoinRequest) Marshal() []byte {
-	buf := make([]byte, 3+16+16)
-	m.Header.Marshal(buf[0:3])
-	copy(buf[3:19], m.Name[:])
-	copy(buf[19:35], m.Token[:])
+	buf := make([]byte, 16+16)
+	m.Place(buf)
 	return buf
 }
 
+func (m *JoinRequest) Place(buf []byte) error {
+	if uint(len(buf)) < m.Size() {
+		return errors.New(ERR_BUFFER_SMALL)
+	}
+	copy(buf[0:16], m.Name[:])
+	copy(buf[16:32], m.Token[:])
+    return nil
+}
+
 func (m *JoinRequest) Unmarshal(buf []byte) error {
-	var err error
 	if uint(len(buf)) != m.Size() {
-		return fmt.Errorf("Not a valid JoinRequest message.")
+		return fmt.Errorf("Not a valid JoinRequest message. (buffered: %v)", len(buf))
 	}
-	err = m.Header.Unmarshal(buf[0:3])
-	if err != nil {
-		return err
-	}
-	copy(m.Name[:], buf[3:19])
-	copy(m.Token[:], buf[19:35])
+	copy(m.Name[:], buf[0:16])
+	copy(m.Token[:], buf[16:32])
 	return nil
 }
 
 func (m *JoinRequest) Size() uint {
-	return uint(3 + 16 + 16)
+	return uint(16 + 16)
 }
 
 // NodeActivate : An existing node is up.
 type NodeActivate struct {
-	Header MessageHeader
-	ID     *uuid.UUID
+	ID uuid.UUID
 }
 
-func NewNodeActivateMessage(id *uuid.UUID) *NodeActivate {
+func NewNodeActivateMessage(id uuid.UUID) *NodeActivate {
 	return &NodeActivate{
-		Header: MessageHeader{
-			Major: VERSION_MAJOR,
-			Minor: VERSION_MINOR,
-			Type:  NODE_ACTIVATE,
-		},
 		ID: id,
 	}
 }
 
-func (m *NodeActivate) GetVersion() (uint8, uint8) {
-	return m.Header.Major, m.Header.Minor
-}
-
 func (m *NodeActivate) Size() uint {
-	return uint(MSG_HEADER_SIZE + binary.Size(m.ID))
+	return uint(binary.Size(m.ID))
 }
 
 func (m *NodeActivate) Type() uint8 {
-	return m.Header.Type
+	return NODE_ACTIVATE
 }
 
 func (m *NodeActivate) Marshal() []byte {
-	buf := make([]byte, m.Size(), m.Size())
-	m.Header.Marshal(buf[0:3])
-	copy(buf[3:3+binary.Size(m.ID)], m.ID[:])
+	buf := make([]byte, m.Size())
+	m.Place(buf)
 	return buf
 }
 
+func (m *NodeActivate) Place(buf []byte) error {
+	if uint(len(buf)) < m.Size() {
+		return errors.New(ERR_BUFFER_SMALL)
+	}
+	copy(buf[0:binary.Size(m.ID)], m.ID[:])
+	return nil
+}
+
 func (m *NodeActivate) Unmarshal(buf []byte) error {
-	if uint(len(buf)) < m.Size() || uint8(buf[2]) != NODE_ACTIVATE {
+	if uint(len(buf)) < m.Size() {
 		return fmt.Errorf("Not a valid NodeActivate message.")
 	}
-	if m.ID == nil {
-		return fmt.Errorf("Null ID Buffer.")
-	}
-	m.Header.Unmarshal(buf[0:3])
-	copy(m.ID[:], buf[3:3+binary.Size(m.ID)])
+	copy(m.ID[:], buf[0:binary.Size(m.ID)])
 	return nil
 }
 
 // Heartbeat : Liveness pulse signal.
 // Subtype: Master Heartbeat, Node Heartbeat
 type Heartbeat struct {
-	Header  MessageHeader
 	NetName [16]byte
 	Master  uuid.UUID
 	Term    uint64
 	Index   uint64
 }
 
-func NewMasterHeartbeat() *Heartbeat {
+func NewHeartbeat() *Heartbeat {
 	m := new(Heartbeat)
-	m.Header.Major = VERSION_MAJOR
-	m.Header.Minor = VERSION_MINOR
-	m.Header.Type = HEARTBEAT_MASTER
 	return m
-}
-
-func NewNodeHeartbeat() *Heartbeat {
-	m := new(Heartbeat)
-	m.Header.Major = VERSION_MAJOR
-	m.Header.Minor = VERSION_MINOR
-	m.Header.Type = HEARTBEAT_NODE
-	return m
-}
-
-func (m *Heartbeat) GetVersion() (uint8, uint8) {
-	return m.Header.Major, m.Header.Minor
 }
 
 func (m *Heartbeat) Type() uint8 {
-	return m.Header.Type
+	return NODE_ACTIVATE
 }
 
 func (m *Heartbeat) Marshal() []byte {
-	buf := make([]byte, m.Size(), m.Size())
-	m.Header.Marshal(buf[0:3])
-	copy(buf[3:19], m.NetName[:])
-	copy(buf[19:35], m.Master[:])
-	binary.BigEndian.PutUint64(buf[35:43], m.Term)
-	binary.BigEndian.PutUint64(buf[43:51], m.Index)
+	buf := make([]byte, m.Size())
+	m.Place(buf)
 	return buf
 }
 
+func (m *Heartbeat) Place(buf []byte) error {
+	if uint(len(buf)) < m.Size() {
+		return errors.New(ERR_BUFFER_SMALL)
+	}
+	copy(buf[0:16], m.NetName[:])
+	copy(buf[16:32], m.Master[:])
+	binary.BigEndian.PutUint64(buf[32:40], m.Term)
+	binary.BigEndian.PutUint64(buf[40:48], m.Index)
+	return nil
+}
+
 func (m *Heartbeat) Unmarshal(buf []byte) error {
-	if uint(len(buf)) < m.Size() || (uint8(buf[2]) != HEARTBEAT_MASTER && uint8(buf[2]) != HEARTBEAT_NODE) {
+	if uint(len(buf)) < m.Size() {
 		return fmt.Errorf("Not a valid Heartbeat message.")
 	}
-	m.Header.Unmarshal(buf[0:3])
-	copy(m.NetName[:], buf[3:19])
-	copy(m.Master[:], buf[19:35])
-	m.Term = binary.BigEndian.Uint64(buf[35:43])
-	m.Index = binary.BigEndian.Uint64(buf[43:51])
+	copy(m.NetName[:], buf[0:16])
+	copy(m.Master[:], buf[16:32])
+	m.Term = binary.BigEndian.Uint64(buf[32:40])
+	m.Index = binary.BigEndian.Uint64(buf[40:48])
 	return nil
 }
 
 func (m *Heartbeat) Size() uint {
 	return uint(binary.Size(*m))
-}
-
-//
-type ConfigInfoRequest struct {
-	Header   MessageHeader
-	CfgIndex uint64
-	Term     uint64
-}
-
-type ConfigResponse struct {
-	Header MessageHeader
 }
--- src/overturn/protocol/packet.go
+++ src/overturn/protocol/packet.go
@@ -10,66 +10,90 @@ import (
 // Packet Encapsulation
 type OVTPacket []byte
 
+// +-----------------+
+// |    magic (+0)   |
+// |   = 'OVT\xAA'   |
+// +--------+--------+
+// | Version|  Type  |
+// |  (+4)  |  (+6)  |
+// +--------+--------+
+// |   Length (+8)   |
+// +-----------------+
+// |  Payload (+12)  |
+// +-----------------+
+
 type OVTEncapsulatedPacket interface {
 	OVTPacketRef() OVTPacket
 	Marshal() []byte
 	Unmarshal([]byte) error
 }
 
-//type OVTPacket struct {
-//	icmp.Message
-//	data []byte
-//}
-
 type TunnelPacket interface {
 	PayloadRef() []byte
 }
 
 var (
-	OVT_MAGIC = [4]byte{'O', 'V', 'T', 0xAA}
+	OVT_VERSION = 0xAA
+	OVT_MAGIC   = [4]byte{'O', 'V', 'T', 0xAA}
+	VERSION = [2]byte{1, 0}
 )
 
 const (
-	OVT_HEADER_SIZE = 8
+	OVT_HEADER_SIZE = 12
 
 	PKG_TOO_SHORT = "Packet too short."
 	PKG_INVALID   = "Not a OVTPacket."
 )
 
-func PlaceNewOVTPacket(buf []byte, payload_size uint) OVTPacket {
+const (
+	RAW_PAYLOAD = iota
+	NODE_ACTIVATE
+	HEARTBEAT_MASTER
+	HEARTBEAT_NODE
+	JOIN_REQUEST
+)
+
+func PlaceNewOVTPacket(buf []byte, payload_size uint, packet_type uint16) OVTPacket {
 	if uint(len(buf)) < payload_size+OVT_HEADER_SIZE {
 		return nil
 	}
 	copy(buf[0:4], OVT_MAGIC[:])
+	copy(buf[4:6], VERSION[:])
+	binary.BigEndian.PutUint16(buf[6:8], packet_type)
 	return OVTPacket(buf)
 }
 
 func OVTPacketUnpack(buf []byte, size_limit uint32) (bool, OVTPacket, error) {
 
-	if len(buf) < 4 {
+	if len(buf) < OVT_HEADER_SIZE {
 		return false, nil, errors.New(PKG_TOO_SHORT)
 	}
 	if bytes.Equal(buf[0:4], OVT_MAGIC[:]) {
 		return false, nil, errors.New(PKG_INVALID)
 	}
-	if len(buf) < 8 {
+	if len(buf) < OVT_HEADER_SIZE {
 		return true, nil, errors.New(PKG_TOO_SHORT)
 	}
-	enc_len := binary.BigEndian.Uint32(buf[4:8])
+	enc_len := binary.BigEndian.Uint32(buf[8:12])
 	if enc_len != uint32(len(buf)) {
-		return true, nil, fmt.Errorf("Payload size unmatched. (buffered: %v, encoded: %v)", len(buf), enc_len)
+		return true, nil, fmt.Errorf("Packet size unmatched. (buffered: %v, encoded: %v)", len(buf), enc_len)
 	}
 	if enc_len > size_limit {
 		return true, nil, fmt.Errorf("Packet size limited for safety.")
 	}
+	return true, OVTPacket(buf), nil
+}
 
-	pkt := make(OVTPacket, enc_len)
-	copy(pkt.PayloadRef(), buf[OVT_HEADER_SIZE:])
-	return true, pkt, nil
+func (pack OVTPacket) PayloadType() uint16 {
+	return binary.BigEndian.Uint16(pack[6:8])
 }
 
 func (pack OVTPacket) MagicRef() OVTPacket {
 	return pack[0:4]
+}
+
+func (pack OVTPacket) Version() (uint8, uint8) {
+	return pack[4], pack[5]
 }
 
 func (pack OVTPacket) Pack() []byte {
@@ -77,10 +101,10 @@ func (pack OVTPacket) Pack() []byte {
 
 	enc_len = uint32(len(pack))
 
-	binary.BigEndian.PutUint32(pack[4:8], enc_len)
+	binary.BigEndian.PutUint32(pack[8:12], enc_len)
 	return []byte(pack)
 }
 
 func (pack OVTPacket) PayloadRef() []byte {
-	return pack[8:]
+	return pack[OVT_HEADER_SIZE:]
 }
